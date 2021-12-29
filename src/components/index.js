55c3b21aaf79f3308b95ce78f7a8bb926042fa7f
adnensaid <adnensaid83@gmail.com>
2021-12-29 16:12:43 +0100

    debug ok

--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,6 @@ export { default as Header } from "./header/Header";
 export { default as Home } from "./home/Home"; 
 export { default as Portfolio } from "./portfolio/Portfolio";
 export { default as Labs } from "./labs/Labs";
-export { default as Filter } from "./filter/Filter";
 
 export { default as Contact } from "./contact/Contact";
 
--- src/components/portfolio/projects/project/Project.js
+++ src/components/portfolio/projects/project/Project.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Style from  "../projects.module.scss";
 import Window from "./window/Window";
 
